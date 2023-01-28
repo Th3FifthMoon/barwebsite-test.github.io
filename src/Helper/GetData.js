c28e5eaae8f8a6fedfe63b32f62fdcb429e3b344
Th3FifthMoon <TheEyeOfMichael@protonmail.com>
2023-01-27 18:01:29 -0800

    updated api call to https

--- src/Helper/GetData.js
+++ src/Helper/GetData.js
@@ -1,4 +1,4 @@
-const getReqURL = 'http://theeyeofmichael.pythonanywhere.com/bar_api/category/';
+const getReqURL = 'https://theeyeofmichael.pythonanywhere.com/bar_api/category/';
 
 export default function getMenu() {
     return getReqURL;
